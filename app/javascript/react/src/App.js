51f9b4860fea714da90f2a32505febac41191d91
Sama Elayan <sama@Samas-MacBook-Pro.local>
2017-10-18 16:56:32 -0400

    Adds Foundation, Bathroom Tile, Bathroom Info, relevant testing, and removed NavBar for now.

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Router, browserHistory, Route, IndexRoute } from 'react-router';
-<<<<<<< HEAD
 import BathroomShowContainer from './containers/BathroomShowContainer'
-=======
->>>>>>> master
 import IndexContainer from './containers/IndexContainer'
 
 
@@ -11,14 +8,11 @@ import IndexContainer from './containers/IndexContainer'
 const App = props => {
   return(
     <Router history={browserHistory}>
-<<<<<<< HEAD
       <Route path='/'>
         <IndexRoute component={IndexContainer} />
         <Route path='/bathrooms/:id'component={BathroomShowContainer} />
       </Route>
-=======
       <Route path='/' component={IndexContainer} />
->>>>>>> master
     </Router>
   )
 }
