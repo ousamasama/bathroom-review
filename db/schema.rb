f28632bffad7195f2cb6ec8b84adde8fef5a48eb
Nick Reid <reidnickb@gmail.com>
2017-10-20 16:36:56 -0400

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020175136) do
+ActiveRecord::Schema.define(version: 20171020201241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 20171020175136) do
     t.datetime "updated_at", null: false
     t.string "role", default: "member", null: false
     t.string "username", null: false
-    t.string "avatar_url"
     t.string "city"
     t.string "state"
     t.string "profile_photo"
