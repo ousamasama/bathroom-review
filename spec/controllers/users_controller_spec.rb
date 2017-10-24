395591224adca5a81f9804686b77c4c84d8ede63
Nick Reid <reidnickb@gmail.com>
2017-10-24 13:54:13 -0400

    Fixes broken test!

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,20 +3,23 @@ require 'rails_helper'
 RSpec.describe "API V1 Users", type: 'request' do
   describe 'DELETE /api/v1/admin/users' do
     context 'as an admin' do
-      let(:user) {FactoryGirl.create(:user, id: 99)}
+      let!(:user) {FactoryGirl.create(:user, id: 99)}
       let(:admin) {FactoryGirl.create(:user, role: "admin")}
 
       it 'deletes a user' do
-        login_as(admin, :scope => :admin)
-        expect { delete "/api/v1/admin/users/99.json" }.to change(User, :count).by(-1)
+        login_as(admin, scope: :user)
+
+        expect { delete "/api/v1/admin/users/#{user.id}" , params: { id: user.id }}.to change(User, :count).by(-1)
       end
 
-      # it "creates a review with the correct attributes" do
-      #   user = bathroom.user
-      #   login_as(user, :scope => :user)
-      #   post "/api/v1/reviews", params: valid_params
-      #   expect(Review.last).to have_attributes valid_params[:review]
-      # end
+
+      it 'fails to delete a user if not an admin' do
+        login_as(user, scope: :user)
+
+        expect { delete "/api/v1/admin/users/#{user.id}" , params: { id: user.id }}.to change(User, :count).by(0)
+      end
+
+
     end
   end
 end
