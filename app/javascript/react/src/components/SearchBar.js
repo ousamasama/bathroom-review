bb4d586562dff0474a9fe691a394afef898aaaae
Nick Reid <reidnickb@gmail.com>
2017-10-25 10:39:00 -0400

    Tried to update foundation version and broke stuff

--- app/javascript/react/src/components/SearchBar.js
+++ app/javascript/react/src/components/SearchBar.js
@@ -2,18 +2,30 @@ import React from 'react'
 
 const SearchBar = props => {
   return(
-    <div>
+    <div className="input-group input-group-rounded">
       <a className="button large" onClick={props.handleClick} id='submit'>Search</a>
       <label>
         <input
+          className="input-group-field"
           id='search-field'
           name='address'
-          type='text'
+          type='search'
           placeholder='Enter location'
           value={props.address}
           onChange={props.handlerFunction}
           />
       </label>
+      <div className="input-group-button">
+        <input type="submit" class="button secondary" value="Search" onClick={props.handleClick}/>
+      </div>
+
+      <div class="input-group input-group-rounded">
+        <input class="input-group-field" type="search"/>
+        <div class="input-group-button">
+          <input type="submit" class="button secondary" value="Search"/>
+        </div>
+      </div>
+
     </div>
 
   );
