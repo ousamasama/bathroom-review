44509a9cbd8d0785add7790ad107ff2a752223c7
Nick Reid <reidnickb@gmail.com>
2017-10-26 18:10:21 -0400

    merges updates from master

--- app/javascript/react/src/components/BathroomTile.js
+++ app/javascript/react/src/components/BathroomTile.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const BathroomTile = props => {
-  return(
-    <div className="bathroom-tile">
-      <h3>{props.bathroom.establishment}</h3>
-      <p className="tile-address">1.2 miles - {props.bathroom.address}</p>
-      <p className="tile-rating">Rating</p>
-      <Link to={`/bathrooms/${props.bathroom.id}`}><div className="button secondary">See more</div></Link>
-    </div>
-  )
+  if (props.searchAddress !== '') {
+    return(
+      <div className="bathroom-tile">
+        <h3>{props.bathroom.establishment}</h3>
+        <p className="tile-address">{props.bathroom.distance} miles away</p>
+        <p className="tile-address">{props.bathroom.address}</p>
+        <p className="tile-rating">Rating: {props.bathroom.review_average}</p>
+        <p className="tile-rating">Total Reviews: {props.bathroom.review_total}</p>
+        <Link to={`/bathrooms/${props.bathroom.id}`}><div className="button secondary">See more</div></Link>
+      </div>
+    )
+  } else {
+     return(
+      <div className="bathroom-tile">
+        <h3>{props.bathroom.establishment}</h3>
+        <p className="tile-address">{props.bathroom.address}</p>
+        <p className="tile-rating">Rating: {props.bathroom.review_average}</p>
+        <p className="tile-rating">Total Reviews: {props.bathroom.review_total}</p>
+        <Link to={`/bathrooms/${props.bathroom.id}`}><div className="button secondary">See more</div></Link>
+      </div>
+    )
+  }
 }
 
 export default BathroomTile;
