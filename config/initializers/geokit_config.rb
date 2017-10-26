837a5ae46a6ec8dc2279939f1f2e5d75ff0c8b0c
Nick Reid <reidnickb@gmail.com>
2017-10-26 14:27:56 -0400

    heroku geokit testing

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -29,7 +29,7 @@ Geokit::Geocoders::request_timeout = 3
 # You can also use the free API key instead of signed requests
 # See https://developers.google.com/maps/documentation/geocoding/#api_key
 # Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GEOKIT_TOKEN']
-Geokit::Geocoders::google = ENV['GEOKIT_TOKEN']
+Geokit::Geocoders::GoogleGeocoder = ENV['GEOKIT_TOKEN']
 Geokit::Geocoders::provider_order=[:google]
 
 # You can also set multiple API KEYS for different domains that may be directed
